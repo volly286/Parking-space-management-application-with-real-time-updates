c249a1c9d38f9709a8f0a21a0785a84af9471580
volly286 <153730319+volly286@users.noreply.github.com>
2025-03-06 16:38:13 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#importuri de biblioteci și module în Python, folosite pentru a construi aplicații web cu Flask și alte funcționalităț
+#importuri de biblioteci și module în Python, folosite pentru a construi aplicații web cu Flask și alte funcționalități
 import re
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_mysqldb import MySQL
